7e234b9c6553cc105c87d0cd0a0b2b1fd82a9d8f
Steve Owino <90975591+StevenOwino@users.noreply.github.com>
2021-10-22 12:38:07 +0300

    Update Primes.hs

--- Primes.hs
+++ Primes.hs
@@ -1,3 +1,9 @@
+-- The language of Arithmetic is BOUNDED in the Natural numbers i.e, requires a decision procedure
+-- Every primitive RECURSIVE FUNCTION is representable in N
+-- Definition of an INTEGRAL: A bounded function with an absolute value greater than Epsilon
+-- The Integral is a required definition to prove Theorems i.e, Knowing what is, and what is not a proof in a System
+
+
 import Data.List
 main = do
     print "HowManyPrimes? - OnlyInteger"
@@ -27,3 +33,8 @@ primesTillN n = 2:[ x | x <- [3,5..n], primeNumber x ]
 firstNPrimes:: Int -> [Int]
 firstNPrimes 0 = []
 firstNPrimes n = 2: take (n-1) [x|x <- [3,5..], primeNumber x]
+
+-- REFERENCE: 
+-- Analysis III - Integration by Professor Ben Green (Oxford University)
+-- Lebesgue's Theory of Integration_Its Origns and Development by Thomas Hawkins
+-- Remarks on the Theory of Integration and Measure by Stephen Owino (https://www.amazon.com/REMARKS-THEORIES-MEASUREMENT-INTEGRATION-LEBESGUE/dp/B093CKNB41)     
