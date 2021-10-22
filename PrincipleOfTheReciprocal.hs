08b4b9da7a4b54afd7318c1ee0aa7c3f75f5625f
Steve Owino <90975591+StevenOwino@users.noreply.github.com>
2021-10-22 20:59:07 +0300

    Add files via upload

---
+++ PrincipleOfTheReciprocal.hs
@@ -0,0 +1,68 @@
+
+-- PRINCPLE OF THE RECIPROCAL(An Essay by Steven Owino)
+
+-- To understand the problem of SELF-ADJOINTNESS for the Momentum operator(P), there is an extra term that does not
+-- satisfy the required rules of relation in Cartesian coordinates i.e, Its not Square-Integrable.
+-- We thus need the COVARIANT DERIVATIVE to solve,
+-- or implement a Complex Valued Function on the total Associated Space of the Principal Bundle,
+-- and not in Euclidean space(R^d),
+-- by eliminitating the extra term by, choosing an Omega(a unit operator) approriately.
+--
+-- A Covariant Derivative acting on the vector field solves the problem of the extra term contrary to a function,
+-- to achieve self-adjointntness (A tensor bundle in the manifold)
+-- The DETERMINANT of the a Bilinear form is NOT scalar, for it picks an INVERSE factor which is an inner-product,
+-- also called an operator, inserted in the vector field[In the case of a one-form base space in the Yang-Mills Field]
+-- for any choice of Coordinates in a Manifold.
+-- For any LOCAL manifold, 
+-- we can now realize the COMMUTATOR RELATION and SELF-ADJOINTNESS on a curved space, since P is WELL-DEFINED.
+-- Here we have 2 REAL Omegas on the GRADIENT FIELD.
+-- 
+-- UTILITY : 
+--
+-- (I)The push-forward of the vector fileld only EXISTS if the map is an ISOMORPHISM 
+-- i.e, the TRANSFORMATION does NOT change the MATRICES(witchcraft) 
+
+-- (II) Conserved Quantities: The POTENTIAL on a Rotating Vector Field on a chosen axis does NOT change
+-- It is an INVARAINT or also termed as a QUADRATURE UNIT OF ROTATION) 
+
+-- (III) Theorems: Every Left-Invariant vector field on a LIE GROUP is complete 
+-- i.e, the DERIVATIVE of the IDENTITY is the TANGENT VECTOR/BUNDLE, and as such, SATISFIES the Commutator Relation.
+
+-- (IV) Differential Topology, where a FUNCTION is a homotopy EQUIVALANCE with respect to the IDENTITY MAP
+
+-- (V) FUNCTIONAL PROGRAMMING 
+-- If a PREDICATE/AXIOMS of a SYSTEM of a left-hand operator is TRUE, 
+-- then the right hand operator cannot affect the result of the LOGICAL-OR operator, and is thus NOT evaluated. 
+-- This causes us to evaluate the "Then" branch.
+-- In a PURE TYPE SYSTEM (Programming Languages) like System F, Coq, Haskell, RUST,..
+-- an ENUMERATION e.g, Graham's Scan Algorithm, is implemented by writing a Function as Series of Equations, 
+-- to get an ordered SET of BOUNDARY points. 
+-- Thus, knowing the signature of the function alone LIMITS the possible valid behaviors it could have and 
+-- makes the JOB of understanding CODE easier.
+-- When we know which part of the code has side effects, and then be appropriately on GUARD.
+--
+-- CONCLUSION: 
+-- Technically, it is necesary for CONSTRUCTIVE LOGIC to reason about proofs and formulas.
+-- In the case above, for the DERIVATIVE of an IDENTITY, there is a need to PRESERVE the condition of an INVERSE, or
+-- a CONJAGUTAE EQUIVALANCE RELATION, and that this condition is trivial in a CARTESIAN COORDINATE/PRODUCT.
+-- Here the LINEAR BASIS is recovered as the KERNEL of an Omega SUBSPACE i.e, DIFFERENTIATED.
+-- H_p(P) = Ker(Omega_p) is defined as 
+-- X being an Element of T_p(P) sent to Omega_p(X_p) = 0 ; The IDENTITY BASIS
+--
+-- The entire STRUCTURE of MATHEMATICS is based on the WELL-DEFINED notion that:
+-- There is ALWAYS a NEGATIVE SIGN in the COMPOSITION of an IDENTITY. i.e, a negative EXPONENT yields a FRACTION,
+-- which is an INVERSE operation, and IMPLIES no further extension to the SYSTEM. 
+--
+-- Clearly, from the Master of us all, Euler gives the DEFINITION of a  LOGARITHM, as the EXPONENT of an INVOLUTION.
+--
+-- AND....
+--
+-- Henri Lebesgue gives the required DEFINITION of an INTEGRAL:
+-- as a FUNCTION with an ABSOLUTE VALUE greater than Epsilon.
+--
+-- The objective of this short essay is to clearly address the need for Functional Programmers to understand 
+-- the PRINCIPLE OF THE RECIPROCAL, the non-commutative relation condition emerging in a Cartesian Coordinate 
+-- for a given Complex-Dimensional Manifold, and the COMPLETE FORMULATION of a TANGENT SPACE.  
+
+
+
